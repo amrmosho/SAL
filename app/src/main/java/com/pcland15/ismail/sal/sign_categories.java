c4e6842505bd2caf2e4e971bb85c3b57a7d8c328
ismail <emcpalnd@gmail.com>
2015-10-22 13:55:35 +0200

    jb

---
+++ app/src/main/java/com/pcland15/ismail/sal/sign_categories.java
@@ -0,0 +1,13 @@
+package com.pcland15.ismail.sal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sign_categories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_categories);
+    }
+}
--- app/src/main/java/com/pcland15/ismail/sal/user_reg.java
+++ app/src/main/java/com/pcland15/ismail/sal/user_reg.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.pcland15.ismail.sal.libs.dbOperations;
 
@@ -36,11 +37,63 @@ public class user_reg extends AppCompatActivity {
     }
 
     public void userRegSend(View view) {
-        email.getText();
+
+
         dbOperations o = new dbOperations("studentes", "insert");
+
+
+
+
+      if (email.getText().toString().equalsIgnoreCase("")){
+          email.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+          Toast t = Toast.makeText(this, "Empty", Toast.LENGTH_LONG);
+          t.show();
+          return;
+
+      }else{
+          email.setTextColor(getResources().getColor(R.color.textColorPrimary));
+
+      }
+
+
+
+        if (title.getText().toString().equalsIgnoreCase("")){
+            title.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+            Toast t = Toast.makeText(this, "Empty", Toast.LENGTH_LONG);
+            t.show();
+            return;
+
+        }else{
+            title.setTextColor(getResources().getColor(R.color.textColorPrimary));
+
+        }
+
+
+
+
         o.addData.put("email", email.getText().toString());
         o.addData.put("title", title.getText().toString());
         o.addData.put("password", password.getText().toString());
-        o.commit();
+
+
+
+
+        HashMap<String, String> a = o.commit();
+        if (a.get("opstauts").equalsIgnoreCase("true")) {
+
+            Toast t = Toast.makeText(this, "Done", Toast.LENGTH_LONG);
+            t.show();
+            email.setText("");
+            title.setText("");
+            password.setText("");
+
+        } else {
+
+            Toast t = Toast.makeText(this, "Error", Toast.LENGTH_LONG);
+            t.show();
+
+        }
+
+
     }
 }

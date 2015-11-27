50a6b800e6bf21fb0517900d1b60b2f03d13ef8e
ismail <emcpalnd@gmail.com>
2015-11-27 19:03:37 +0200

    jb

--- app/src/main/java/com/pcland15/ismail/sal/libs/config.java
+++ app/src/main/java/com/pcland15/ismail/sal/libs/config.java
@@ -4,17 +4,13 @@ package com.pcland15.ismail.sal.libs;
  * Created by empcl_000 on 19/10/2015.
  */
 public class config {
-    public  static  String  webServiecURL="http://sign.decorplanner.com/webserves/";
-    public  static  String  imagePath="http://sign.decorplanner.com/upload/";
-    public  static  String  vidoePath="http://sign.decorplanner.com/upload/";
+    public  static  String  webServiecURL="http://tabukasl.com/webserves/";
+    public  static  String  imagePath="http://tabukasl.com/upload/";
+    public  static  String  vidoePath="http://tabukasl.com/upload/";
     /*
     http://sign.decorplanner.com/webserves/?set=get_row&table=studentes
     http://192.168.1.5/sign
 
      */
-
-
-
-
 
 }
